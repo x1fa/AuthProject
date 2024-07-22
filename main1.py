80148252d126a60cb1efce7bf16b5f58a01284f7
x1fa <x1fa@qq.com>
2024-07-23 00:42:47 +0800

    1

--- main1.py
+++ main1.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 import cpuinfo
 import psutil
@@ -6,29 +7,42 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QTextEdit, QGridLayout, QLineEdit
 
 
-def get_cpu_info():
-    info = cpuinfo.get_cpu_info()
-    return info['brand_raw']  # 获取 CPU 型号作为部分指纹
+def get_cpu_id():
+    # 使用 subprocess 来调用 wmic 命令
+    cmd = 'wmic cpu get ProcessorId'
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True,
+                               creationflags=subprocess.CREATE_NO_WINDOW)
+    out, err = process.communicate()
+    if process.returncode == 0:
+        # 解析输出结果
+        # WMIC 输出格式通常包括标题行和实际数据行，我们需要解析实际的数据行
+        output = out.decode().strip().split('\n')
+        # 第一行通常是标题，第二行是数据
+        if len(output) > 1:
+            return output[1].strip()  # 返回处理后的 CPU ID
+        else:
+            return "No CPU ID found"
+    else:
+        return f"Error retrieving CPU ID: {err.decode().strip()}"
 
 
-def get_disk_info():
-    # 获取第一个磁盘的设备名
-    for disk in psutil.disk_partitions():
-        if 'fixed' in disk.opts:
-            return disk.device
-    return None
+# def get_disk_info():
+#     # 获取第一个磁盘的设备名
+#     for disk in psutil.disk_partitions():
+#         if 'fixed' in disk.opts:
+#             return disk.device
+#     return None
 
 
 def generate_fingerprint():
     # 组合获取的信息生成机器指纹
-    cpu_info = get_cpu_info()
-    disk_info = get_disk_info()
+    cpu_info = get_cpu_id()
     text_info = 'douyin'
-    if None in (cpu_info, disk_info, text_info):
+    if None in (cpu_info, text_info):
         raise ValueError("Failed to get all necessary hardware IDs.")
 
     # 创建原始指纹字符串
-    raw_fingerprint = f"{cpu_info} | {disk_info} | {text_info}"
+    raw_fingerprint = f"{cpu_info}|{text_info}"
     # 使用 SHA-256 哈希算法生成指纹
     fingerprint = hashlib.sha256(raw_fingerprint.encode()).hexdigest()
     return fingerprint
@@ -60,7 +74,7 @@ class App(QWidget):
         self.textEdit.setText(fingerprint)
 
 
-def main():
+def open_window():
     app = QApplication(sys.argv)
     ex = App()
     ex.show()
@@ -68,4 +82,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    open_window()
